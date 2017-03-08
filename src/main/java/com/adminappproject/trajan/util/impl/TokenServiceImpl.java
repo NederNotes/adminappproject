47225e4bc6124fe0438ce53550900dd51b47f6c5
Brian.Lee <brainjohnlee06@gmail.com>
2017-03-08 16:14:50 +0800

    set stable commit for master branch

--- src/main/java/com/adminappproject/trajan/util/impl/TokenServiceImpl.java
+++ src/main/java/com/adminappproject/trajan/util/impl/TokenServiceImpl.java
@@ -1,20 +1,4 @@
 package com.adminappproject.trajan.util.impl;
 
-import org.springframework.security.core.token.Token;
-import org.springframework.security.core.token.TokenService;
-
-public class TokenServiceImpl implements TokenService {
-
-	@Override
-	public Token allocateToken(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Token verifyToken(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+public class TokenServiceImpl {
 }
