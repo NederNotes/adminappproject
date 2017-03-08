c59c4e60991f309898491e2b85cdc979e20c1739
Brian.Lee <brainjohnlee06@gmail.com>
2017-03-08 16:09:00 +0800

    Revert last commit to change security implementation to tokenize string

--- src/main/java/com/adminappproject/trajan/config/MainInstanceInitiator.java
+++ src/main/java/com/adminappproject/trajan/config/MainInstanceInitiator.java
@@ -3,7 +3,6 @@ package com.adminappproject.trajan.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -14,7 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 		"com.adminappproject.trajan.validator" })
 @EnableJpaRepositories("com.adminappproject.trajan.repo")
 @EntityScan("com.adminappproject.trajan.model")
-public class MainInstanceInitiator extends SpringBootServletInitializer {
+public class MainInstanceInitiator {
 	public static void main(String[] args) {
 		SpringApplication.run(MainInstanceInitiator.class, args);
 	}
--- src/main/java/com/adminappproject/trajan/service/SecurityService.java
+++
@@ -1,9 +0,0 @@
-package com.adminappproject.trajan.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface SecurityService {
-	public String findLoggedInUsername();
-	public void autologin(String username, String password);
-}
